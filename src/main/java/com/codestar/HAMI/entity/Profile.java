0ee51851eb793ff10a9f7ea0befb9117c94cc598
amirack <amirack003@gmail.com>
2023-08-08 16:53:32 +0330

    feat: Add user and profile entities.

---
+++ src/main/java/com/codestar/HAMI/entity/Profile.java
@@ -0,0 +1,40 @@
+package com.codestar.HAMI.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "profiles")
+@Setter
+@Getter
+public class Profile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique=true, nullable = false, length = 40)
+    @Size(min = 3, max = 40)
+    private String username;
+
+    @Column(length = 60, nullable = false)
+    @NotEmpty
+    @NotBlank
+    @Size(min = 8, max = 50)
+    private String firstName;
+
+    @Column(length = 60)
+    private String lastName;
+
+    @Column(length = 100)
+    private String bio;
+
+    private byte[] picture;
+
+    @ManyToOne
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user;
+}
--- src/main/java/com/codestar/HAMI/entity/User.java
+++ src/main/java/com/codestar/HAMI/entity/User.java
@@ -1,4 +1,29 @@
 package com.codestar.HAMI.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Setter @Getter
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique=true, nullable = false)
+    @Email
+    private String email;
+
+    @Column(nullable = false, length = 50)
+    @Size(min = 8, max = 50)
+    private String password;
+
+    @OneToMany
+    private Set<Profile> profiles;
 }
