57bfed9f24e9f28cd8c221710bdc82c4e157a107
Hossein <hossein.ghermezcheshme@gmail.com>
2023-08-14 14:17:09 +0330

    add swagger.

--- src/main/java/com/codestar/HAMI/HamiApplication.java
+++ src/main/java/com/codestar/HAMI/HamiApplication.java
@@ -2,12 +2,29 @@ package com.codestar.HAMI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class HamiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HamiApplication.class, args);
+    }
+
+    @Bean
+    public Docket postsApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo()).select().build();
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Hermes API")
+                .description("API for developers")
+                .termsOfServiceUrl("https://simplifyingtechcode.wordpress.com/")
+                .licenseUrl("simplifyingtech@gmail.com").version("2.0").build();
     }
 
 }
